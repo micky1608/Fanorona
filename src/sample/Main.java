355295a22428796df34015f483131f3c68740e9b
Mickael <mteaudors@outlook.com>
2018-02-20 21:08:14 +0100

    add icon for the game

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,6 +22,7 @@ public class Main extends Application {
         Parent root = loader.load();
         this.controller = loader.getController();
         primaryStage.setTitle("Fanorona");
+        primaryStage.getIcons().add(new Image("/images/icone.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
